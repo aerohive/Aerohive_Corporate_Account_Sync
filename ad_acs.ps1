6522c25b3f35f7bbe11801cea074992214a1263d
Thomas Munzer <thomas.munzer@gmail.com>
2017-08-11 14:48:02 +0200

    code improvments and new features
    new help
    new tect function
    new checkOnly function

--- ad_acs.ps1
+++ ad_acs.ps1
@@ -1,116 +1,179 @@
-﻿<#--------------------------------------------------------------
+﻿
+param(
+    [Parameter(Mandatory=$false)] 
+    [alias ('file', 'f')]
+    [string]$configFile = $PSScriptRoot + "\settings.ini",
+
+    [Parameter(Mandatory=$false)] 
+    [alias ('test', 't')]
+    [string]$testUser,
+
+    [Parameter(Mandatory=$false)] 
+    [alias ('checkOnly', 'c')]
+    [switch]$doNotCreate,
+
+    [Parameter(Mandatory=$false)] 
+    [alias ('help', 'h')]
+    [switch]$showHelp
+)
+
+<#--------------------------------------------------------------
 LOAD SETTINGS
 --------------------------------------------------------------#>
-$settings=$PSScriptRoot + "\settings.ini"
-Get-Content $settings | foreach-object -begin {$params=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True) -and ($k[0].StartsWith('#') -ne $True)) { $params.Add($k[0], $k[1].Trim()) } }
+function LoadSettings(){
+    Write-Host "Loading parameters from $configFile"
+    $params = @{}
+    Get-Content $configFile | foreach-object -process {
+        $k = [regex]::split($_, '=')
+        if (($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True) -and ($k[0].StartsWith('#') -ne $True)) { 
+            $params.Add($k[0], $k[1].Trim().Trim('"').Trim("'")) 
+        } 
+    }
+    $script:clientId = $params.clientId
+    $script:clientSecret = $params.clientSecret
+    $script:redirectUrl = $params.redirectUrl
+    $script:vpcUrl = $params.vpcUrl
+    $script:accessToken = $params.accessToken
+    $script:refreshToken = $params.refreshToken
+    $script:expireDate = $params.expireDate
+    $script:ownerId = $params.ownerId
+    $script:acsUserGroupId = $params.acsUserGroupId
+    $script:acsUserName = $params.acsUserName.ToString()
+    $script:acsEmail = $params.acsEmail
+    $script:acsPhone = $params.acsPhone
+    $script:acsOrganization = $params.acsOrganization
+    if ($params.acsDeliveryMethod -like "NO_DELIVERY" -or 
+        $params.acsDeliveryMethod -like "EMAIL" -or
+        $params.acsDeliveryMethod -like "SMS" -or
+        $params.acsDeliveryMethod -like "EMAIL_AND_SMS") 
+    {
+        $script:acsDeliveryMethod = $params.acsDeliveryMethod
+    } else {
+        Write-Error "Wrong 'acsDeliveryMethod' parameter. Please correct it and start again. Exiting..."
+        exit 254
+    }
+    $script:adGroup = $params.adGroup
+    $script:logFile = $params.logFile
+    try {$script:logToAFile = [System.Convert]::ToBoolean($params.logToAFile)}
+    catch {
+        Write-Error "Wrong 'logToAFile' parameter. Please correct it and start again. Exiting..."
+        exit 254
+    }
+    try {$script:logToConsole = [System.Convert]::ToBoolean($params.logToConsole)}
+    catch {
+        Write-Error "Wrong 'logToConsole' parameter. Please correct it and start again. Exiting..."
+        exit 254
+    }
+    if ($params.logLevel -like "debug" -or $params.logLevel -like "info" -or $params.logLevel -like "error") {$script:logLevel = $params.logLevel}
+    else {
+        Write-Error "Wrong 'logLevel' parameter. Please correct it and start again. Exiting..."
+        exit 254
+    }
 
-$clientId=$params.clientId
-$clientSecret=$params.clientSecret
-$redirectUrl=$params.redirectUrl
-$vpcUrl=$params.vpcUrl
-$accessToken=$params.accessToken
-$refreshToken=$params.refreshToken
-$expireDate=$params.expireDate
-$ownerId=$params.ownerId
-$acsUserGroupId = $params.acsUserGroupId
-$acsUserName = $params.acsUserName.ToString()
-$acsEmail = $params.acsEmail
-$acsPhone = $params.acsPhone
-$acsOrganization = $params.acsOrganization
-$acsDeliveryMethod = $params.acsDeliveryMethod
-$adGroup=$params.adGroup
-$logFile=$params.logFile
-$logToAFile=$params.logToAFile
-$logToConsole=$params.logToConsole
-
-
+    $script:headers = @{
+        "X-AH-API-CLIENT-SECRET"       = "$clientSecret";
+        "X-AH-API-CLIENT-ID"           = "$clientId";
+        "X-AH-API-CLIENT-REDIRECT-URI" = "$redirectUrl";
+        "Authorization"                = "Bearer $accessToken"
+    }
+}
 <#--------------------------------------------------------------
 SCRIPT
 --------------------------------------------------------------#>
-$headers=@{
-    "X-AH-API-CLIENT-SECRET"="$clientSecret";
-    "X-AH-API-CLIENT-ID"="$clientId";
-    "X-AH-API-CLIENT-REDIRECT-URI"="$redirectUrl";
-    "Authorization"="Bearer $accessToken"
-}
 
 ###############################################################
 ######### LOGGING
 ###############################################################
-function Log-Error ($mess) {
+function LogError($mess) {
     $date = Get-Date -Format d
     $time = Get-Date -Format HH:mm:ss.fff
     $logstring = "$date $time ERROR: $mess"
     if ($logToAFile) {Add-content $logFile -value $logstring}
     if ($logToConsole) {Write-Host $logstring -ForegroundColor Red}
 }
-function Log-Info ($mess) {
-    $date = Get-Date -Format d
-    $time = Get-Date -Format HH:mm:ss.fff
-    $logstring = "$date $time INFO: $mess"
-    if ($logToAFile) {Add-content $logFile -value $logstring}
-    if ($logToConsole) {Write-Host $logstring -ForegroundColor Green}
+function LogInfo ($mess) {
+    if ($logLevel -like "debug" -or $logLevel -like "info") {
+        $date = Get-Date -Format d
+        $time = Get-Date -Format HH:mm:ss.fff
+        $logstring = "$date $time INFO: $mess"
+        if ($logToAFile) {Add-content $logFile -value $logstring}
+        if ($logToConsole) {Write-Host $logstring -ForegroundColor Green}
+    }
 }
-function Log-Debug($mess) {
-    $date = Get-Date -Format d
-    $time = Get-Date -Format HH:mm:ss.fff
-    $logstring = "$date $time DEBUG: $mess"
-    if ($logToAFile) {Add-content $logFile -value $logstring}
-    if ($logToConsole) {Write-Host $logstring -ForegroundColor Gray}
+function LogDebug($mess) {
+    if ($logLevel -like "debug") {
+        $date = Get-Date -Format d
+        $time = Get-Date -Format HH:mm:ss.fff
+        $logstring = "$date $time DEBUG: $mess"
+        if ($logToAFile) {Add-content $logFile -value $logstring}
+        if ($logToConsole) {Write-Host $logstring -ForegroundColor Gray}
+    }
 }
 
 ######################################################
 ######### ACS Requests Functions
 ######################################################
-function AcsError($data){
-        Log-Error("Got HTTP" + $data.error.status + ": "+$data.error.code)
-        Log-Error("Message: " + $data.error.message)
+
+function AcsError($data) {
+    LogError("Got HTTP" + $data.error.status + ": " + $data.error.code)
+    LogError("Message: " + $data.error.message)
 }
 function GetUsersFromAcs() {
     try { 
         $response = (Invoke-RestMethod -Uri "https://$vpcUrl/xapi/v1/identity/credentials?ownerId=$ownerId&userGroup=$acsUserGroupId" -Headers $headers -Method Get)
-    } catch {   
-        Log-Error("Can't retrieve Users from ACS")
+    }
+    catch {   
+        LogError("Can't retrieve Users from ACS")
         AcsError(ConvertFrom-Json $_.ErrorDetails.Message)
-        Log-Error("Exiting...")
+        LogError("Exiting...")
         exit 255
     } 
     return $response.data
 }
 
-function CreateAcsAccount($adUser){
-    Log-Info("Creating " + $adUser.$acsUserName)
-    $acsUser=@{
-        "userName"=$adUser.$acsUserName;
-        "email"= $adUser.$acsEmail;
-        "organization"= $adUser.$acsOrganization;
-        "phone"= $adUser.$acsPhone;
-        "firstName"= $acsUser.Name;
-        "groupId"= $acsUserGroupId;
-        "deliverMethod"= $acsDeliveryMethod;
-        "policy"= "PERSONAL";
-        "purpose"= "AD User"
-    }
-    $json = $acsUser | ConvertTo-Json
-    try {
-        $response = Invoke-RestMethod -Uri "https://$vpcUrl/xapi/v1/identity/credentials?ownerId=$ownerId" -Method Post -Headers $headers -Body $json -ContentType "application/json"
-    } catch {
-        Log-Error("Can't create new User " + $adUser.$acsUserName)
-        AcsError(ConvertFrom-Json $_.ErrorDetails.Message)
+function CreateAcsAccount($adUser) {
+    if ($doNotCreate){
+        LogInfo("CHECK ONLY! The account " + $adUser.$acsUserName + " should be deleted" )
+    } else {
+        LogInfo("Creating " + $adUser.$acsUserName)
+        $acsUser = @{
+            "userName"      = $adUser.$acsUserName;
+            "email"         = $adUser.$acsEmail;
+            "organization"  = $adUser.$acsOrganization;
+            "phone"         = $adUser.$acsPhone;
+            "firstName"     = $acsUser.Name;
+            "groupId"       = $acsUserGroupId;
+            "deliverMethod" = $acsDeliveryMethod;
+            "policy"        = "PERSONAL";
+            "purpose"       = "AD User"
+        }
+        $json = $acsUser | ConvertTo-Json
+        try {
+            $response = Invoke-RestMethod -Uri "https://$vpcUrl/xapi/v1/identity/credentials?ownerId=$ownerId" -Method Post -Headers $headers -Body $json -ContentType "application/json"
+        }
+        catch {
+            LogError("Can't create new User " + $adUser.$acsUserName)
+            AcsError(ConvertFrom-Json $_.ErrorDetails.Message)
+        }
     }
     return $response
 }
 
-function DeleteAcsAccount($acsUser){
-    Log-Info("Deleting " + $acsUser.userName + " with Id " + $acsUser.id)
-    $acsUserId = $acsUser.id
-    try {
-        $response = Invoke-RestMethod -Uri "https://$vpcUrl/xapi/v1/identity/credentials?ownerId=$ownerId&ids=$acsUserId" -Method Delete -Headers $headers
-    } catch {
-        Log-Error("Can't delete the User " + $acsUser.userName)
-        AcsError(ConvertFrom-Json $_.ErrorDetails.Message)
+function DeleteAcsAccount($acsUser) {
+    if ($doNotCreate){
+        LogInfo("CHECK ONLY! The account " + $acsUser.userName + " with Id " + $acsUser.id + " should be deleted" )
+    } else {
+        LogInfo("Deleting " + $acsUser.userName + " with Id " + $acsUser.id)
+        $acsUserId = $acsUser.id
+        try {
+            $response = Invoke-RestMethod -Uri "https://$vpcUrl/xapi/v1/identity/credentials?ownerId=$ownerId&ids=$acsUserId" -Method Delete -Headers $headers
+        }
+        catch {
+            LogError("Can't delete the User " + $acsUser.userName)
+            AcsError(ConvertFrom-Json $_.ErrorDetails.Message)
+        }
+        return $response
     }
-    return $response
 }
 
 ######################################################
@@ -119,9 +182,8 @@ function DeleteAcsAccount($acsUser){
 function GetUsersFromAd() {
     $adAccounts = @()
     $users = Get-ADGroupMember $adGroup -Recursive 
-    foreach ($user in $users)
-    {
-        $temp = Get-ADUser $user.SamAccountName -Properties $acsUserName,$acsEmail,$acsOrganization,$acsPhone
+    foreach ($user in $users) {
+        $temp = Get-ADUser $user.SamAccountName -Properties $acsUserName, $acsEmail, $acsOrganization, $acsPhone
         if ($temp) { 
             $adAccounts += $temp    
         }
@@ -129,53 +191,113 @@ function GetUsersFromAd() {
     return $adAccounts
 }
 
-######################################################
-######### Entry point
-######################################################
-
-$acsUsers = GetUsersFromAcs
-$adUsers = GetUsersFromAd
-$validAcsUsers = @()
-$i=0
-foreach ($adUser in $adUsers)
-{
-    $acsAccountExists = $false
-    foreach ($acsUser in $acsUsers) {
-        if ($adUser.$acsUserName -like $acsUser.userName -And $adUser.Enabled -like "False") {
-            $mess = $adUser.$acsUserName + " is disabled. Should be removed"
-            Log-Debug($mess)
-            DeleteAcsAccount($acsUser)
-            break     
-        } elseif ($adUser.$acsUserName -like $acsUser.userName -And $adUser.Enabled -like "True") {
-            $acsAccountExists = $true
-            $validAcsUsers += $acsUser
-            break
-        } 
+function TestUser() {
+    $users = Get-ADGroupMember $adGroup -Recursive 
+    foreach ($user in $users){
+        if ($user.SamAccountName -like "$testUser") {
+                Get-ADUser $user.SamAccountName -Properties $acsUserName, $acsEmail, $acsOrganization, $acsPhone
+            }
     }
-    if (-not $acsAccountExists) {
-        $mess = $adUser.$acsUserName + " doesn't have any PPSK. Should be created"
-        Log-Debug($mess)
-        CreateAcsAccount($adUser)
-    } else {
-        $mess = $adUser.$acsUserName + " is enabled and already has a PPSK. nothing to do"
-        Log-Debug($mess)
-    }
-    $i++
-    $percentage = (($i / $adUsers.length)  * 100)
-    Write-Progress -activity "Checking AD Users" -status "Progress: " -PercentComplete $percentage  -CurrentOperation "$percentage%"
-  Start-Sleep 1
-
 }
 
-
-$i = 0
-foreach ($acsUser in $acsUsers) {
-    if ( $validAcsUsers -notcontains $acsUser) {
-        $mess= $acsUSer.userName + " should be removed because it does not belong to the AD"
-        Log-Debug($mess)
-        DeleteAcsAccount($acsUser)
+######################################################
+######### start process
+######################################################
+function StartProcess(){
+    $acsUsers = GetUsersFromAcs
+    $adUsers = GetUsersFromAd
+    $validAcsUsers = @()
+    $i = 0
+    foreach ($adUser in $adUsers) {
+        $acsAccountExists = $false
+        foreach ($acsUser in $acsUsers) {
+            if ($adUser.$acsUserName -like $acsUser.userName -And $adUser.Enabled -like "False") {
+                $mess = $adUser.$acsUserName + " is disabled. Should be removed"
+                LogDebug($mess)
+                DeleteAcsAccount($acsUser)
+                break     
+            }
+            elseif ($adUser.$acsUserName -like $acsUser.userName -And $adUser.Enabled -like "True") {
+                $acsAccountExists = $true
+                $validAcsUsers += $acsUser
+                break
+            } 
+        }
+        if (-not $acsAccountExists) {
+            $mess = $adUser.$acsUserName + " doesn't have any PPSK. Should be created"
+            LogDebug($mess)
+            CreateAcsAccount($adUser)
+        }
+        else {
+            $mess = $adUser.$acsUserName + " is enabled and already has a PPSK. nothing to do"
+            LogDebug($mess)
+        }
+        $i++
+        $percentage = (($i / $adUsers.length) * 100)
+        Write-Progress -activity "Checking AD Users" -status "Progress: " -PercentComplete $percentage  -CurrentOperation "$percentage%"
     }
-    $i++
-    $percentage = (($i / $acsUsers.length)  * 100)
-    Write-Progress -activity "Checking Aerohive Users" -status "Progress: " -PercentComplete $percentage  -CurrentOperation "$percentage%"
+
+
+    $i = 0
+    foreach ($acsUser in $acsUsers) {
+        if ( $validAcsUsers -notcontains $acsUser) {
+            $mess = $acsUSer.userName + " should be removed because it does not belong to the AD"
+            LogDebug($mess)
+            DeleteAcsAccount($acsUser)
+        }
+        $i++
+        $percentage = (($i / $acsUsers.length) * 100)
+        Write-Progress -activity "Checking Aerohive Users" -status "Progress: " -PercentComplete $percentage  -CurrentOperation "$percentage%"
+    }
+}
+
+######################################################
+######### usage
+######################################################
+function Usage(){
+    $usage=@"
+NAME
+        Aerohive Corporate Account Sync
+
+SYNOPSIS
+        acas.ps1 [OPTION [WORD]]
+
+DESCRIPTION
+        Aerohive Corporate Account Sync is a PowerShell Script using ACS 
+        APIs to automate the creation of User Accounts on HiveManager NG
+        for domain users.
+
+options are
+        -h
+        -help               This help.
+
+        -f <file>
+        -file <file>        Path to the configuration file. By default, 
+                            the script will try to find the settings.ini 
+                            file in script location.
+
+        -t 
+        -test <ad_user>     Test the AD configuration and display the 
+                            available fields.
+
+        -c
+        -checkOnly          Only check the accounts. When the -c flag is 
+                            present, the script will not create/remove
+                            the the account form HiveMaager NG.
+        
+
+"@
+    Write-Host $usage
+}
+######################################################
+######### entry point
+######################################################
+
+if ($showHelp) {Usage}
+elseif ($testUser) {TestUser}
+else {
+    LoadSettings
+    LogInfo("Starting process")
+    StartProcess
+    LogInfo("Process finisehd")
 }
